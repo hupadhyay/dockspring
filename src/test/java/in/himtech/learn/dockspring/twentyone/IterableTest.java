2c299ebf4f97b5453ac69b978eacfdd6df121e65
hupadhyay <himanshu2703@gmail.com>
2024-02-12 22:43:36 -0600

    Adding of github action yml file

---
+++ src/test/java/in/himtech/learn/dockspring/twentyone/IterableTest.java
@@ -0,0 +1,24 @@
+package in.himtech.learn.dockspring.twentyone;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IterableTest {
+
+    record Employee(String name, String company){};
+
+    @Test
+    public void testArrayList(){
+        List<Employee> list = new ArrayList<>();
+        list.add(new Employee("Rakesh", "L&T"));
+
+        List<Employee> list2 = new ArrayList<>();
+        list2.add(new Employee("Rakesh", "L&T"));
+        System.out.println(list2);
+
+        Assertions.assertIterableEquals(list, list2);
+    }
+}
