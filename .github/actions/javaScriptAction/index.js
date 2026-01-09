f4dcbd6667cdf4fed59e66bd733f3726f96c8b62
himanshu <himanshu2703@gmail.com>
2026-01-08 19:10:55 -0600

    Adding dependencies for javascript action

--- .github/actions/javaScriptAction/index.js
+++ .github/actions/javaScriptAction/index.js
@@ -1,25 +1,23 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 
-function run() {
-    try {
-      // `name-greet` input defined in action metadata file
-      core.info("JavaScript Action is running!");
-      const nameToGreet = core.getInput("name-greet");
-      core.info(`Hello ${nameToGreet}!`);
+try {
+  // `name-greet` input defined in action metadata file
+  core.info("JavaScript Action is running!");
+  const nameToGreet = core.getInput("name-greet");
+  core.info(`Hello ${nameToGreet}!`);
 
-      // Get the current time and set it as an output variable
-      const time = new Date().toTimeString();
-      core.setOutput("time-of-greet", time);
+  // Get the current time and set it as an output variable
+  const time = new Date().toTimeString();
+  core.setOutput("time-of-greet", time);
 
-      // Get the JSON webhook payload for the event that triggered the workflow
-      const payload = JSON.stringify(github.context.payload, undefined, 2);
-      core.info(`The event payload: ${payload}`);
-    } catch (error) {
-      core.info("Action failed");
-      core.setFailed(error.message);
-    }
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  core.info(`The event payload: ${payload}`);
+} catch (error) {
+  core.info("Action failed");
+  core.setFailed(error.message);
 }
 
 
-run();
+
